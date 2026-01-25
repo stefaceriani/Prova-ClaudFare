559ebf98483fc60cf8d39f523eb4bc7f845b7af3
stefaceriani <stefano.ceriani@blu.it>
2026-01-25 11:04:55 +0100

    Add files via upload

--- theme-toggle.js
+++ theme-toggle.js
@@ -3,4 +3,7 @@ const btn = document.getElementById("theme-toggle");
 btn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     document.body.classList.toggle("light");
+
+    btn.textContent = document.body.classList.contains("dark") ? "🔆" : "🌒";
+
 });
