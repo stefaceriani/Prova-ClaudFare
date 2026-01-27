b2c474b0c84319d83245a683f2f0f2c3fc95cbc0
stefaceriani <stefano.ceriani@blu.it>
2026-01-27 21:58:48 +0100

    Delete theme-toggle.js

--- theme-toggle.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("theme-toggle");
-
-btn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    document.body.classList.toggle("light");
-
-    btn.textContent = document.body.classList.contains("dark") ? "🔆" : "🌒";
-
-});
